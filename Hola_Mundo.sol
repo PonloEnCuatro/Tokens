2636a3d2857027f5b0cd6498f70d123aa312b0b9
PonloEnCuatro <oscardavidre2017@gmail.com>
2024-09-09 19:49:36 -0500

    Se crea primer contrato en avalanche

---
+++ Hola_Mundo.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Primer contrato inteligente (hola mundo)
+
+pragma solidity 0.8.24;
+
+contract hola_mundo {
+    string public saludo = "Hola mundo desde mi casa xd";
+}
