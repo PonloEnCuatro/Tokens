9c747fc8a6398e9d59c42ee98d03b0f68a05dcf5
PonloEnCuatro <oscardavidre2017@gmail.com>
2024-09-16 18:54:03 -0500

    Se brindo la solucion al problema (pendiente hacerlo a mi manera)

--- libreta.sol
+++ libreta.sol
@@ -1,18 +1,11 @@
 // SPDX-License-Identifier: MIT
-/*
 
-Este contrato guarda los siguientes datos en la blockchain
-- nombre
-- correo electronico
-- numero de telefono
-
-En numeros
-
-*/
 pragma solidity 0.8.24;
 
 contract libreta{
     string nombre = "Pablo Perez";
+    string correo = "nn@mail.com";
+    uint256 telefono = 1234567890;
 
     function guardar_nombre(string memory _nombre) public {
         nombre = _nombre;
@@ -20,5 +13,21 @@ contract libreta{
 
     function leer_nombre() public view returns (string memory){
         return nombre;
+    }
+
+    function guardar_correo(string memory _correo) public {
+        correo = _correo;
+    }
+
+    function leer_correo()public view returns (string memory) {
+        return correo;
+    }
+
+    function guardar_telefono(uint256 _telefono) public {
+        telefono = _telefono;
+    }
+
+    function leer_telefono() public view returns (uint256) {
+        return telefono;
     }
 }
